83307566bcaddf89c90e44db2d9828a1ba05f8f1
firozrahisuddinsaifi <firozkhandelhi62@gmail.com>
2024-08-18 00:55:05 +0530

    tech stack skills added

--- firozportfolio/src/Components/TechStack.jsx
+++ firozportfolio/src/Components/TechStack.jsx
@@ -1,28 +1,30 @@
-import { TechCardsContainer } from "./TechStack.styles"
+import { TechCardsContainer, TechHeading, TechImageContainer, TechSkillContainer, TechStax } from "./TechStack.styles"
 
 
 export const TechStack=()=>{
 
-    return(<>
+    return<>
         <div>
 
             <div>
-                <div className="stax">
-                    <h1>Tech Stacks</h1>
-                </div>
+                <TechSkillContainer>
+                    <TechHeading>Tech Stacks</TechHeading>
+                </TechSkillContainer>
 
                 <TechCardsContainer>
-                <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrBIch_Bsl-Ea2qIXbpQh8cqTqsWZOHbhuww&s" /><span></span></div>
-                <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfkMYb955fj7IRiw-8g6gmn5GoZzKni1Kv8g&s" /><span></span></div>
-                <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAXaZgOP8Dt5J05vGncrxXkY6VJKoi1_lsFw&s" /><span></span></div>
-                <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCEj_V6_aIHeHJk9LAEq1kk5Bs7bjAzb82qw&s" /><span></span></div>
-                <div><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" /><span></span></div>
-                <div><img src="" /><span></span></div>
-                <div><img src="" /><span></span></div>
-                <div><img src="" /><span></span></div>
+                <TechStax><TechImageContainer src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrBIch_Bsl-Ea2qIXbpQh8cqTqsWZOHbhuww&s" alt="html_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfkMYb955fj7IRiw-8g6gmn5GoZzKni1Kv8g&s" alt="css_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAXaZgOP8Dt5J05vGncrxXkY6VJKoi1_lsFw&s" alt="js_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCEj_V6_aIHeHJk9LAEq1kk5Bs7bjAzb82qw&s" alt="git_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="gitHub_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="" alt="c/c++_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="" alt="DSA_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="" alt="Dbms_Image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="" alt="sql_image" /><span></span></TechStax>
+                <TechStax><TechImageContainer src="" alt="OS_Image" /><span></span></TechStax>
                 </TechCardsContainer>
             </div>
 
         </div>
-    </>)
+    </>
 }
--- firozportfolio/src/Components/TechStack.styles.jsx
+++ firozportfolio/src/Components/TechStack.styles.jsx
@@ -2,15 +2,37 @@ import styled from "styled-components";
 import Colors from "../Colors";
 
 
+export const TechSkillContainer=styled.h1`
+ width:22.2rem;
+ height:auto;
+ margin-left:3%;
+`
+export const TechHeading=styled.div`
+text-transform:uppercase;
+
+`
 
 export const TechCardsContainer=styled.div`
 width:94%;
 margin:auto;
-background:${Colors.darkGray};
+background:${Colors.white};
 display:flex;
-justify-content:center;
+flex-wrap: wrap;
+flex-direction: row;
 align-items:center;
+justify-content: space-around;
+border-radius:0.1rem;
 
+`
 
+export const TechStax=styled.div`
+margin:1rem;
+width:5.2rem;
+height:auto;
+
+`
+
+export const TechImageContainer=styled.img`
+width:100%;
 
 `
